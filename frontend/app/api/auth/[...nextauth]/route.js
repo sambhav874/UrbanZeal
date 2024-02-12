d829c0ac9841f78af166205702fae36bb6cf6257
sambhav874 <sambhavjain874@gmail.com>
2024-02-13 01:02:18 +0530

    minor error in @next-auth/route : isAdmin fixed.

--- frontend/app/api/auth/[...nextauth]/route.js
+++ frontend/app/api/auth/[...nextauth]/route.js
@@ -54,13 +54,17 @@ export async function isAdmin() {
   const session = await getServerSession(authOptions);
   const userEmail = session?.user?.email;
   if (!userEmail) {
+    
     return false;
   }
   const userInfo = await UserInfo.findOne({email:userEmail});
+
   if (!userInfo) {
     return false;
   }
+  
   return userInfo.admin;
+  
 }
 
 const handler = NextAuth(authOptions);
