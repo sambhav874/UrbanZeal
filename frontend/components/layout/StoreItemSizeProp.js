cd1e104773684a439ae4142d668d38bfd410e77c
sambhav874 <sambhavjain874@gmail.com>
2024-02-22 23:30:11 +0530

    updated @StoreItemSizeProps

--- frontend/components/layout/StoreItemSizeProp.js
+++ frontend/components/layout/StoreItemSizeProp.js
@@ -1,7 +1,13 @@
 import Trash from './../../components/icons/Trash';
 import Plus from './../../components/icons/Plus';
+import ChevronDown from './../../components/icons/ChevronDown'
+import ChevronUp from './../../components/icons/ChevronUp'
+import { useState } from 'react';
+
 
 export default function StoreItemSizeProp({ name, addLabel, props, setProps }) {
+
+    const [isOpen , setIsOpen] = useState(false);
 
     function addProp(ev) {
         ev.preventDefault();
@@ -32,11 +38,19 @@ export default function StoreItemSizeProp({ name, addLabel, props, setProps }) {
 
   return (
     <div className='bg-gray-200 p-2 rounded-md mb-2'>
-      <label>Sizes</label>
-      {props?.length > 0 && props.map((size, index) => (
+        <button type='button' onClick={() => {setIsOpen(prev => !prev)}} className='inline-flex p-1 border-0 justify-start'>
+            {isOpen && (<ChevronUp />)}
+            {!isOpen && (<ChevronDown />)}<span>{name}</span>
+            <span>({props?.length})</span>
+            </button>
+        
+      
+<div className={isOpen ? 'block' : 'hidden'}>
+
+{props?.length > 0 && props.map((size, index) => (
         <div key={index} className='flex gap-2 items-end'>
           <div>
-            <div><h3 className='text-gray-800'>{name}</h3></div>
+            <div><label className='text-gray-800'>Name</label></div>
             
             <input
               onChange={ev => editProp(ev, index, 'name')}
@@ -67,6 +81,9 @@ export default function StoreItemSizeProp({ name, addLabel, props, setProps }) {
           <span>{addLabel}</span>
         </button>
       </div>
+
+</div>
+      
     </div>
   );
 }
