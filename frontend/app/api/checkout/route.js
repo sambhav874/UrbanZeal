1ca48d85637a255ab020ea2c5607a1f946df87a1
sambhav874 <sambhavjain874@gmail.com>
2024-03-14 23:07:15 +0530

    fixing payment route --error with stripe

--- frontend/app/api/checkout/route.js
+++ frontend/app/api/checkout/route.js
@@ -1,8 +1,68 @@
 import mongoose from "mongoose"
+import { getServerSession } from "next-auth";
+import { Order } from "../../../models/Order";
+import StoreItem from "../../../components/store/StoreItem";
 const stripe = require('stripe')(process.env.STRIPE_SK)
 
 export async function POST(){
     mongoose.connect(process.env.MONGO_URI);
 
     const {cartProducts , address} = await req.json();
+
+   
+
+    const session = await getServerSession(authOptions);
+    const userEmail = session?.user?.email;
+
+    const orderDoc = await Order.create({
+        userEmail,
+        ...address,
+        cartProducts,
+        paid: false
+    })
+
+    const stripeLineItems = [];
+    for (const cartProduct of cartProducts){
+        const productName = product.name;
+        const productInfo = await StoreItem.findById(cartProduct._id);
+        const productPrice =  cartProduct.price;
+        
+        if(cartProduct.size){
+           const size =  productInfo.sizes.find(size =>size._id.toString() === cartProduct._id.toString());
+           productPrice += size.price;
+        }
+        stripeLineItems.push({
+            quantity: 1 ,
+            price_data: {
+                currency:'INR',
+                price_data: {
+                    name: productName
+                },
+                unit_amount : productPrice * 100
+            }
+        })
+    }
+
+    console.log({stripeLineItems});
+    return Response.json(null);
+
+
+    const stripeSession = await stripe.session.create({
+        line_items: [],
+          mode: 'payment',
+          customer_email: userEmail,
+          success_url: process.env.NEXTAUTH_URL + 'cart?success=1',
+          cancel_url: process.env.NEXTAUTH_URL + 'cart?cancelled=1',
+          metadata: {orderId : orderDoc._id},
+          shipping_options: [
+              {
+                  shipping_rate_data: {
+                      display_name: 'Delivery Fee',
+                      type: 'fixed_amount',
+                      fixed_amount: {amount: 500 , currency:'INR'},
+                  }
+              }
+          ]
+       })
 }
+
--- frontend/app/cart/page.jsx
+++ frontend/app/cart/page.jsx
@@ -4,84 +4,104 @@ import { useContext, useEffect, useState } from "react";
 import Image from "next/image";
 import AddressInputs from './../../components/layout/AddressInputs'
 import Trash from './../../components/icons/Trash'
-import {useProfile} from './../../components/UseProfile'
+import { useProfile } from './../../components/UseProfile'
 
 const CartPage = () => {
     const [address, setAddress] = useState({});
-    const { cartProducts , removeCartProduct } = useContext(CartContext);
-    const {data : profileData} = useProfile();
+    const { cartProducts, removeCartProduct } = useContext(CartContext);
+    const { data: profileData } = useProfile();
 
     useEffect(() => {
 
         if (profileData?.city) {
-            const {phoneNumber , streetAddress , city , pincode , country } = profileData;
-            const addressFromProfile = {phoneNumber , streetAddress , city , pincode , country }
-            setAddress(addressFromProfile);}
-    } , [profileData])
+            const { phoneNumber, streetAddress, city, pincode, country } = profileData;
+            const addressFromProfile = { phoneNumber, streetAddress, city, pincode, country }
+            setAddress(addressFromProfile);
+        }
+    }, [profileData])
 
-let total = 0 ;
-    for (const product of cartProducts){
-        total += cartProductPrice(product);
+    let subtotal = 0;
+    for (const product of cartProducts) {
+        subtotal += cartProductPrice(product);
     }
 
-    function handleAddressChange(propName , value){
-        setAddress(prevAddress => ({...prevAddress , [propName] : value }))
+    function handleAddressChange(propName, value) {
+        setAddress(prevAddress => ({ ...prevAddress, [propName]: value }))
     }
-  
+
+
+    async function proceedToCheckout(ev) {
+
+        ev.preventDefault();
+        const response = await fetch('/api/checkout', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                address,
+                cartProducts
+            })
+        })
+        const link = await response.json();
+        //window.location = link;
+        
+    }
+console.log({ cartProducts })
     return (
-      <section className="my-8">
-        <div className="text-center">
-          <h1 className="text-white text-4xl font-bold">Cart</h1>
-        </div>
-  
-        <div className="mt-8 grid gap-8 grid-cols-2">
-          <div>
-            {cartProducts.length === 0 ? (
-              <div>No products in the cart.</div>
-            ) : (
-              cartProducts.map((products , index) => (
-                <div  className="flex gap-4 mb-4 border-b items-center py-2">
-                    <div className="w-120">
-                        <Image src={products.image} width={240} height={180} alt="" />
-                        </div>  {/* Add unique key for each product */}
-                        <h2>{products.name} ${products.price}</h2>
-                        {products?.size && (
-                            <div className="text-sm text-gray-400">Size : <span>{products.size.name}</span></div>
-                        )}
+        <section className="my-8">
+            <div className="text-center">
+                <h1 className="text-white text-4xl font-bold">Cart</h1>
+            </div>
 
-<div className="text-lg font-semibold">
-                    Rs.{cartProductPrice(products)}
-                     </div>
+            <div className="mt-8 grid gap-8 grid-cols-2">
+                <div>
+                    {cartProducts.length === 0 ? (
+                        <div>No products in the cart.</div>
+                    ) : (
+                        cartProducts.map((products, index) => (
+                            <div className="flex gap-4 mb-4 border-b items-center py-2">
+                                <div className="w-120">
+                                    <Image src={products.image} width={240} height={180} alt="" />
+                                </div>  {/* Add unique key for each product */}
+                                <h2>{products.name} ${products.price}</h2>
+                                {products?.size && (
+                                    <div className="text-sm text-gray-400">Size : <span>{products.size.name}</span></div>
+                                )}
 
-                     <div className="ml-2">
-                    <button className="p-2" onClick={() =>removeCartProduct(index)}>
-                            <Trash />
-                    </button>
-                     </div>
-                  
+                                <div className="text-lg font-semibold">
+                                    Rs.{cartProductPrice(products)}
+                                </div>
+
+                                <div className="ml-2">
+                                    <button className="p-2" onClick={() => removeCartProduct(index)}>
+                                        <Trash />
+                                    </button>
+                                </div>
+
+                            </div>
+
+
+                        ))
+                    )}
+                    <div className="py-4 text-right pr-16">
+                        <span className="text-gray-500">Subtotal :</span>
+                        <span className="text-lg font-semibold">Rs.{subtotal}</span> </div>
+                    <div className="py-4 text-right pr-16">
+                        <span className="text-gray-500">Delivery :</span>
+                        <span className="text-lg font-semibold">Rs.30</span> </div>
                 </div>
+                <div className="bg-gray-300 p-4 m-4 rounded-lg duration-1000 hover:bg-gray-200"> <h2>Checkout</h2>
+                    <form>
 
-                
-              ))
-            )}
-            <div className="py-4 text-right pr-16">
-                <span className="text-gray-500">Subtotal :</span>
-            <span className="text-lg font-semibold">Rs.{total}</span> </div>
-          </div>
-          <div className="bg-gray-300 p-4 m-4 rounded-lg duration-1000 hover:bg-gray-200"> <h2>Checkout</h2> 
-        <form>
+                        <AddressInputs addressProps={address} setAddressProps={handleAddressChange} />
+                        <button type="submit" onSubmit={proceedToCheckout} className="text-slate-700 p-2">
+                            Pay Rs.{subtotal + 30}/-
+                        </button>
+                    </form>
 
-        <AddressInputs addressProps={address} setAddressProps={handleAddressChange}/>
-            <button type="submit" className="text-slate-700 p-2">
-                Pay Rs.{total}/-
-            </button>
-        </form>
-          
-          </div>
-        </div>
-      </section>
+                </div>
+            </div>
+        </section>
     );
-  };
-  
-  export default CartPage;
-  
+};
+
+export default CartPage;
