b02c6ec7a853db231d7e3207efdabfcdd79c4746
sambhav874 <sambhavjain874@gmail.com>
2024-04-07 22:43:39 +0530

    made changes in /products/women

--- frontend/app/products/women/page.jsx
+++ frontend/app/products/women/page.jsx
@@ -1,15 +1,14 @@
 'use client'
-
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import Carousel from '../../../components/Carousel';
-
 import StoreItem from '../../../components/store/StoreItem';
+import './../style.css'
 
 const Women = () => {
   const [subCategories, setSubCategories] = useState([]);
   const [items, setItems] = useState([]);
-  const [carousel , setCarousel] = useState([]);
+  const [carousel, setCarousel] = useState([]);
 
   useEffect(() => {
     fetch('/api/subcategories')
@@ -18,45 +17,40 @@ const Women = () => {
         setSubCategories(subCategory);
       });
 
-      fetch('/api/store-items')
+    fetch('/api/store-items')
       .then(res => res.json())
       .then(items => {
-        // Filter items with category 'Men'
-        const menItems = items.filter(item => item.category === 'Women' ); 
-        setItems(menItems.slice(-5));
+        // Filter items with category 'Women'
+        const womenItems = items.filter(item => item.category === 'women');
+        setItems(womenItems.slice( 0, 5)); 
+        console.log(items)// Assuming you want the latest 5 items
       });
-  
-      fetch('/api/carousel')
-  .then(res => res.json())
-  .then(carouselImages => {
-    const womenImages = carouselImages.womenImages.map(womenImage => womenImage.womenImageUrl);
-    console.log(womenImages); 
-    setCarousel(womenImages);
-  })
-  .catch(error => console.error('Error fetching men images:', error));
 
-
-
+      
+    fetch('/api/carousel')
+      .then(res => res.json())
+      .then(carouselImages => {
+        const womenImages = carouselImages.womenImages.map(womenImage => womenImage.womenImageUrl);
+        setCarousel(womenImages);
+      })
+      .catch(error => console.error('Error fetching women images:', error));
   }, []);
 
   
 
   return (
-    <div className="container mx-auto mt-8">
-      <div className="text-3xl font-bold mb-4">Women</div>
+    <div className="max-w-4xl mx-auto mt-8 px-4 text-center">  <div className="text-4xl font-bold mb-8 store">Women</div>
       <Carousel images={carousel} />
 
-      <h1 className="text-3xl font-semibold mb-4">Our categories</h1>
-      <div className="grid grid-cols-3 gap-4">
+      <h2 className="text-3xl font-semibold  store my-12">Our categories</h2>
+      <div className="grid grid-cols-3 gap-8 ">
         {subCategories.map(subcategory => (
           <div key={subcategory._id}>
-            <Link href={`/products/Women/${subcategory.name}`} >
-              <div className="border border-gray-200 rounded-md overflow-hidden shadow-md w-full">
-                <img src={subcategory.image} alt={subcategory.name} className="w-full h-64 object-cover" />
-                <div className="p-4">
-                  <h3 className="text-lg font-semibold mb-2">{subcategory.name}</h3>
-                  <p className="text-gray-600 mb-2">{subcategory.description}</p>
-                  <button className="bg-blue-500 text-white px-4 py-2 rounded-md mt-4 hover:bg-blue-600">View Products</button>
+            <Link href={`/products/Women/${subcategory.name}`}>
+              <div className="border border-gray-200 rounded-md overflow-hidden shadow-md p-4">  <img src={subcategory.image} alt={subcategory.name} className="w-full h-60 object-cover" />
+                <div className="mt-4">  <h3 className="text-xl font-semibold mb-2">{subcategory.name}</h3>
+                  <p className="text-gray-700 mb-2">{subcategory.description}</p>
+                  <button className="bg-blue-500 text-white px-4 py-2 rounded-md mt-2 hover:bg-blue-600">View Products</button>
                 </div>
               </div>
             </Link>
@@ -64,11 +58,12 @@ const Women = () => {
         ))}
       </div>
 
-      {/* Store Items */}
-      <div className="text-3xl font-bold mb-4">Store Items</div>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+      <div  className="text-3xl font-bold font-serif mt-12 mb-6 store">Store Items</div>
+      <div className="flex flex-row overflow-scroll m-2 ">
         {items.map(product => (
-          <StoreItem key={product._id} {...product} />
+          <div className='p-4 store'>
+          <StoreItem  key={product._id} {...product} />
+          </div>
         ))}
       </div>
     </div>
