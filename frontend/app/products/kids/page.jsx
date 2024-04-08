8400df95d572f2392adb65f2310b1be7440f223d
sambhav874 <sambhavjain874@gmail.com>
2024-04-08 23:30:55 +0530

    updated categories page

--- frontend/app/products/kids/page.jsx
+++ frontend/app/products/kids/page.jsx
@@ -1,63 +1,58 @@
 'use client'
-
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import Carousel from '../../../components/Carousel';
-
 import StoreItem from '../../../components/store/StoreItem';
+import '../style.css'; // Import your stylesheet
 
 const Kids = () => {
+  'use client'; // Add the 'use client' directive if needed
+
   const [subCategories, setSubCategories] = useState([]);
   const [items, setItems] = useState([]);
-  const [carousel , setCarousel] = useState([]);
+  const [carousel, setCarousel] = useState([]);
 
   useEffect(() => {
     fetch('/api/subcategories')
       .then(res => res.json())
       .then(subCategory => {
-        
-        const  subCatArray = subCategory.filter(subcat =>  subcat.parentCategory === 'Kids');
+        const subCatArray = subCategory.filter(subcat => subcat.parentCategory === 'Kids');
         setSubCategories(subCatArray);
       });
 
-      fetch('/api/store-items')
+    fetch('/api/store-items')
       .then(res => res.json())
       .then(items => {
-        // Filter items with category 'Men'
-        const kidsItems = items.filter(item => item.category === 'Kids' ); 
-        setItems(kidsItems.slice(-5));
+        // Filter items with category 'Kids'
+        const kidsItems = items.filter(item => item.category === 'kids');
+        setItems(kidsItems.slice(-5)); // Assuming you want the latest 5 items
       });
-  
-      fetch('/api/carousel')
-  .then(res => res.json())
-  .then(carouselImages => {
-    const kidsImages = carouselImages.kidsImages.map(kidsImage => kidsImage.womenImageUrl);
-    console.log(kidsImages); 
-    setCarousel(kidsImages);
-  })
-  .catch(error => console.error('Error fetching men images:', error));
 
-
+    fetch('/api/carousel')
+      .then(res => res.json())
+      .then(carouselImages => {
+        const kidsImages = carouselImages.kidsImages.map(kidsImage => kidsImage.kidsImageUrl);
+        setCarousel(kidsImages);
+      })
+      .catch(error => console.error('Error fetching kids images:', error));
   }, []);
 
-  
-
   return (
-    <div className="container mx-auto mt-8">
-      <div className="text-3xl font-bold mb-4">Kids</div>
+    <div className="container mx-auto mt-8 px-4 text-center"> {/* Use 'use client' here if not added at the beginning */}
+      <div className="text-4xl font-bold mb-8 store">Kids</div>
       <Carousel images={carousel} />
 
-      <h1 className="text-3xl font-semibold mb-4">Our categories</h1>
-      <div className="grid grid-cols-3 gap-4">
+      <h2 className="text-3xl font-semibold store my-12">Our categories</h2>
+      <div className="grid grid-cols-3 gap-8">
         {subCategories.map(subcategory => (
           <div key={subcategory._id}>
-            <Link href={`/products/Kids/${subcategory.name}`} >
-              <div className="border border-gray-200 rounded-md overflow-hidden shadow-md w-full">
-                <img src={subcategory.image} alt={subcategory.name} className="w-full h-64 object-cover" />
-                <div className="p-4">
-                  <h3 className="text-lg font-semibold mb-2">{subcategory.name}</h3>
-                  <p className="text-gray-600 mb-2">{subcategory.description}</p>
-                  <button className="bg-blue-500 text-white px-4 py-2 rounded-md mt-4 hover:bg-blue-600">View Products</button>
+            <Link href={`/products/Kids/${subcategory.name}`}>
+              <div className="border border-gray-200 rounded-md overflow-hidden shadow-md p-4">
+                <img src={subcategory.image} alt={subcategory.name} className="w-full h-60 object-cover" />
+                <div className="mt-4">
+                  <h3 className="text-xl font-semibold mb-2">{subcategory.name}</h3>
+                  <p className="text-gray-700 mb-2">{subcategory.description}</p>
+                  <button className="bg-blue-500 text-white px-4 py-2 rounded-md mt-2 hover:bg-blue-600">View Products</button>
                 </div>
               </div>
             </Link>
@@ -65,11 +60,12 @@ const Kids = () => {
         ))}
       </div>
 
-      {/* Store Items */}
-      <div className="text-3xl font-bold mb-4">Store Items</div>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+      <div className="text-3xl font-bold font-serif mt-12 mb-6 store">Store Items</div>
+      <div className="flex flex-row overflow-scroll m-2">
         {items.map(product => (
-          <StoreItem key={product._id} {...product} />
+          <div className="p-4 store">
+            <StoreItem key={product._id} {...product} />
+          </div>
         ))}
       </div>
     </div>
--- frontend/app/products/men/page.jsx
+++ frontend/app/products/men/page.jsx
@@ -1,9 +1,9 @@
 'use client'
-
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import Carousel from '../../../components/Carousel';
 import StoreItem from '../../../components/store/StoreItem';
+import '../style.css'; // Import your stylesheet
 
 const Men = () => {
   const [subCategories, setSubCategories] = useState([]);
@@ -14,49 +14,43 @@ const Men = () => {
     fetch('/api/subcategories')
       .then(res => res.json())
       .then(subCategory => {
-        
-        const  subCatArray = subCategory.filter(subcat =>  subcat.parentCategory === 'Men');
+        const subCatArray = subCategory.filter(subcat => subcat.parentCategory === 'Men');
         setSubCategories(subCatArray);
       });
 
-      fetch('/api/store-items')
+    fetch('/api/store-items')
       .then(res => res.json())
       .then(items => {
         // Filter items with category 'Men'
-        const menItems = items.filter(item => item.category === 'Men' ); 
-        setItems(menItems.slice(-5));
+        const menItems = items.filter(item => item.category === 'Men');
+        setItems(menItems.slice(-5)); // Assuming you want the latest 5 items
       });
 
-      fetch('/api/carousel')
-  .then(res => res.json())
-  .then(carouselImages => {
-    const menImages = carouselImages.menImages.map(menImage => menImage.menImageUrl);
-    console.log(menImages); 
-    setCarousel(menImages);
-  })
-  .catch(error => console.error('Error fetching men images:', error));
-
+    fetch('/api/carousel')
+      .then(res => res.json())
+      .then(carouselImages => {
+        const menImages = carouselImages.menImages.map(menImage => menImage.menImageUrl);
+        setCarousel(menImages);
+      })
+      .catch(error => console.error('Error fetching men images:', error));
   }, []);
 
-
-  
-
   return (
-    <div className="container mx-auto mt-8">
-      <div className="text-3xl font-bold mb-4">Mens</div>
+    <div className="container mx-auto mt-8 px-4 text-center"> {/* Use 'use client' here if needed */}
+      <div className="text-4xl font-bold mb-8 store">Men</div>
       <Carousel images={carousel} />
 
-      <h1 className="text-3xl font-semibold mb-4">Our categories</h1>
-      <div className="grid grid-cols-3 gap-4">
+      <h2 className="text-3xl font-semibold store my-12">Our categories</h2>
+      <div className="grid grid-cols-3 gap-8">
         {subCategories.map(subcategory => (
           <div key={subcategory._id}>
-            <Link href={`/products/Men/${subcategory.name}`} >
-              <div className="border border-gray-200 rounded-md overflow-hidden shadow-md w-full">
-                <img src={subcategory.image} alt={subcategory.name} className="w-full h-64 object-cover" />
-                <div className="p-4">
-                  <h3 className="text-lg font-semibold mb-2">{subcategory.name}</h3>
-                  <p className="text-gray-600 mb-2">{subcategory.description}</p>
-                  <button className="bg-blue-500 text-white px-4 py-2 rounded-md mt-4 hover:bg-blue-600">View Products</button>
+            <Link href={`/products/Men/${subcategory.name}`}>
+              <div className="border border-gray-200 rounded-md overflow-hidden shadow-md p-4">
+                <img src={subcategory.image} alt={subcategory.name} className="w-full h-60 object-cover" />
+                <div className="mt-4">
+                  <h3 className="text-xl font-semibold mb-2">{subcategory.name}</h3>
+                  <p className="text-gray-700 mb-2">{subcategory.description}</p>
+                  <button className="bg-blue-500 text-white px-4 py-2 rounded-md mt-2 hover:bg-blue-600">View Products</button>
                 </div>
               </div>
             </Link>
@@ -64,11 +58,12 @@ const Men = () => {
         ))}
       </div>
 
-      {/* Store Items */}
-      <div className="text-3xl font-bold mb-4">Store Items</div>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+      <div className="text-3xl font-bold font-serif mt-12 mb-6 store">Store Items</div>
+      <div className="flex flex-row overflow-scroll m-2">
         {items.map(product => (
-          <StoreItem key={product._id} {...product} />
+          <div className="p-4 store">
+            <StoreItem key={product._id} {...product} />
+          </div>
         ))}
       </div>
     </div>
