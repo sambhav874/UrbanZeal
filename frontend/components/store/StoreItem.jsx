bcb717242e10c7fd42b10a945c8e5a4b343231c5
sambhav874 <sambhavjain874@gmail.com>
2024-04-11 22:53:12 +0530

    fixed @storeItem

--- frontend/components/store/StoreItem.jsx
+++ frontend/components/store/StoreItem.jsx
@@ -6,7 +6,7 @@ import StoreItemTile from "./../store/StoreItemTile";
 import Image from "next/image";
 
 const StoreItem = (storeItem) => {
-  const { image, name, price, description, sizes } = storeItem;
+  const { image, name, price, description, sizes, category } = storeItem;
   const [selectedSize, setSelectedSize] = useState(sizes?.[0] || null);
   const [showPopUp, setShowPopUp] = useState(false);
   const { addToCart } = useContext(CartContext);
@@ -27,32 +27,29 @@ const StoreItem = (storeItem) => {
     <>
       {showPopUp && (
         <div onClick={() => setShowPopUp(false)} className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-80">
-          <div onClick={ev => ev.stopPropagation()} className="flex flex-col p-6 m-3 space-y-10 bg-slate-500 rounded-2xl shadow-2xl md:flex-row md:space-y-0 md:space-x-10 md:m-0 md:p-16">
-            <div className="p-2 mx-1 mt-1 pb-4 space-x-10 items-center flex rounded-xl bg-slate-800">
+          <div onClick={ev => ev.stopPropagation()} className="flex flex-col p-4 m-2 space-y-8 bg-white rounded-2xl shadow-2xl md:flex-row md:space-y-0 md:space-x-8 md:m-0 md:p-10 max-w-3xl">
+            
               <Image
                 src={image}
                 alt={name + " image"}
-                height={200}
-                width={350}
-                className="mx-auto hover:scale-105 mb-4 w-96 flex bg-white rounded-xl text-white hover:bg-violet-700 duration-1000 hover:bg-animate-pulse shadow-black shadow-2xl"
+                height={100}
+                width={270}
+                className="mx-auto hover:scale-105 mb-4 bg-white rounded-xl text-white duration-1000 hover:bg-animate-pulse shadow-black shadow-2xl"
               />
-            </div>
-            <div className="flex-col flex space-y-4">
-              <div className="flex flex-col mb-4 space-y-3 text-center md:text-left">
+            
+            <div className="flex-col  space-y-4">
+              <div className="flex-col mb-4 space-y-3 text-center md:text-left">
                 <div className="inline-block px-3 py-1 text-sm text-white bg-black rounded-full">
-                  Free Shipping
+                  {category}
                 </div>
                 <div className="max-w-sm text-2xl font-medium">{name}</div>
                 <div className="flex flex-col mb-2 space-y-3 text-center md:text-left">
                   <p className="text-5xl font-bold">Rs .{price}/-</p>
                   <p className="text-sm font-light text-gray-200 dark:text-black">
-                    This offer is available till 7 June or as long as the stock
-                    lasts.
+                  {description}
                   </p>
                 </div>
-                <div className="overflow-y-scroll">
-                  <p className="text-center text-gray-300 text-md mb-2">{description}</p>
-                </div>
+                
                 {sizes?.length > 0 && (
                   <div className="text-center md:text-left">
                     <h3 className="text-sm font-semibold text-gray-700">
@@ -99,4 +96,3 @@ const StoreItem = (storeItem) => {
 };
 
 export default StoreItem;
-
